ef5ee315f621579a95b5394afa8221ddd92c4de1
vpkarthi50 <vpkarthi50@gmail.com>
2022-10-19 21:46:33 +0530

    js datatype

---
+++ js-datatype/js/main.js
@@ -0,0 +1,34 @@
+//js start
+//js variable declaration model
+
+var a="hi"
+console.log(a);
+
+let b="hello"
+	b="prabhakaran"
+console.log(b);
+
+const c="vanakam"
+	//c="vanthanam"
+console.log(c);
+
+
+//js datatype declaraion primitive datatypes
+
+let d="karthi"
+console.log(typeof(d));
+
+let e=51
+console.log(typeof(e));
+
+let f=true
+console.log(typeof(f));
+
+let g;
+console.log(typeof(g));
+
+let h=null;
+console.log(typeof (h) + " Null is treated as an object so it shows a objject inn typeeof" );
+
+let i=123456789n;
+console.log(typeof(i));
