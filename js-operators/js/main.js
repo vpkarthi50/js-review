e16ea8c9e85bb8c52c5f42bb383c25cc58dfe871
vpkarthi50 <vpkarthi50@gmail.com>
2022-11-01 14:34:14 +0530

    js-operators

---
+++ js-operators/js/main.js
@@ -0,0 +1,89 @@
+//js start
+
+console.log("Today We About Operator");
+
+console.log("They are 5 opertators");
+
+console.log("---Assignment Operator---");
+
+console.log("---Arithmetic Operator---");
+
+console.log("---Comparison Operator---");
+
+console.log("---Logical Operator---");
+
+console.log("---Bitwise Operator---");
+
+console.log("<---Assignment Operator--->");
+
+console.log("---This operator use to assign a value---");
+
+let a=10;
+	a+=5;
+	b=15;
+	b-=5;
+console.log(a);
+
+console.log(b); //Assignment Operator
+
+console.log("<---Arithmetic Operator--->");
+
+console.log("---This operator use to Do arithmetic operations---");
+
+let c=3;
+let	d="add"	;
+console.log(c+d + " Addition");//Addition
+
+console.log(c-d + " Subtraction");//Subtraction
+
+console.log(c*d + " Multiplication");//Multiplication
+
+console.log(c/d + " Divide");//Divide
+
+console.log(c%d + " remainder");//remainder
+
+console.log(c**d + " Expotential");//Expotential
+
+
+console.log("<---Comparision Operator--->");
+
+console.log("---This operator use to Do comapare a values---");
+
+console.log((c==d) + " Equal to");//Equal to
+
+console.log((c===d) + " Equal value");//Equal to
+
+console.log((c!=d) + " Not equal");//Not equal
+
+console.log((c!==d) + " Not equal value");//Not equal value
+
+console.log((c>d) + " Greater than");//Greater than
+
+console.log((c<d) + " Less than");//Lessthan than
+
+console.log((c<=d) + " Less than Equal to");//Less than Equal to
+
+let age=21;
+
+let vote=age>=21 ?"Eligible for Marriage":"Note eligible for Marriage"
+
+console.log(vote + " -------Ternary operator------");// Ternary operator
+
+
+console.log("<---Logical Operator--->");
+
+console.log("---This operator use to Do Logical operations---");
+
+let num1=5,num2=4,num3=5,num4=4;
+
+console.log("&&--And Operator--&&");
+
+console.log((num1>num2)&&(num3<num4));
+
+console.log("||--OR Operator--||");
+
+console.log((num1>num2)||(num3<num4));
+
+console.log("!!--Not Operator--!!");
+
+console.log(!(num1>num2)||(num3<num4));
